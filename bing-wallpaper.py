b6c469e30d3001b779a07632fa415212ce52615d
Paweł J <jpawz@outlook.com>
2023-10-29 21:32:37 +0100

    Dowanload image as wallpaper.jpg

--- bing-wallpaper.py
+++ bing-wallpaper.py
@@ -8,5 +8,4 @@ BingJSON_URL = BingUrl + "HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pl-PL"
 with urllib.request.urlopen(BingJSON_URL) as url:
     data = json.load(url)
     imgUrl = data["images"][0]["url"]
-    startdate = data["images"][0]["startdate"]
-    urlretrieve(BingUrl + imgUrl, startdate + ".jpg")
+    urlretrieve(BingUrl + imgUrl, "wallpaper.jpg")
