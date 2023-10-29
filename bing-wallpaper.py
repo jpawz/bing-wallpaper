46c8d21bcef1eb93b9b13c78d9d418b79ff8cfe2
Paweł J <jpawz@outlook.com>
2023-10-29 21:10:18 +0100

    Download bing image of the day into current directory

---
+++ bing-wallpaper.py
@@ -0,0 +1,12 @@
+import json
+import os
+import urllib
+from urllib.request import urlopen, urlretrieve
+
+BingUrl = "https://www.bing.com/"
+BingJSON_URL = BingUrl + "HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pl-PL"
+with urllib.request.urlopen(BingJSON_URL) as url:
+    data = json.load(url)
+    imgUrl = data["images"][0]["url"]
+    startdate = data["images"][0]["startdate"]
+    urlretrieve(BingUrl + imgUrl, startdate + ".jpg")
