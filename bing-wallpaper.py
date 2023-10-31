275245f23f9c01972b650776f7dee5ae6a6858fc
Paweł J <jpawz@outlook.com>
2023-10-31 20:35:46 +0100

    Switch to urllib3 with retries

--- bing-wallpaper.py
+++ bing-wallpaper.py
@@ -1,11 +1,17 @@
 import json
-import os
-import urllib
-from urllib.request import urlopen, urlretrieve
+import urllib3
 
+retries = urllib3.Retry(total=5, backoff_factor=10)
+http = urllib3.PoolManager(retries=retries)
 BingUrl = "https://www.bing.com/"
 BingJSON_URL = BingUrl + "HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pl-PL"
-with urllib.request.urlopen(BingJSON_URL) as url:
-    data = json.load(url)
+response = http.request("GET", BingJSON_URL)
+
+if response.status == 200:
+    data = json.loads(response.data.decode("utf-8"))
     imgUrl = data["images"][0]["url"]
-    urlretrieve(BingUrl + imgUrl, "wallpaper.jpg")
+
+    wallpaper = http.request("GET", BingUrl + imgUrl)
+    if wallpaper.status == 200:
+        with open("wallpaper.jpg", "wb") as file:
+            file.write(wallpaper.data)
